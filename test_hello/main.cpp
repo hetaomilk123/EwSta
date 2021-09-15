8bb09bda080a6891dd6f42c84522e8af135d1458
hetaomilk123 <hetaomilk123@126.com>
2021-09-15 15:59:43 +0800

    update main.cpp and add build.sh

--- test_hello/main.cpp
+++ test_hello/main.cpp
@@ -4,5 +4,14 @@ using namespace std;
 int main()
 {
 	cout << "hello, hetaomilk!" << endl;
+	int *p = NULL;
+
+	{
+		int val = 20;
+		p = &val;
+		cout << "*p in local: " << *p << endl;
+	}	
+
+	cout << "*p out local: " << *p << endl;
 	return 0;
 }
