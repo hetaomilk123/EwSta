5cb228e699bd7783727d98269f219b5c0813dcb0
hetaomilk123 <hetaomilk123@126.com>
2021-09-15 19:14:14 +0800

    update main.cpp

--- test_hello/main.cpp
+++ test_hello/main.cpp
@@ -1,3 +1,5 @@
+# if 0
+// Test 2
 #include <iostream>
 using namespace std;
 
@@ -13,5 +15,39 @@ int main()
 	}	
 
 	cout << "*p out local: " << *p << endl;
+	return 0;
+}
+#endif
+
+#if 0
+// Test 3
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+int main()
+{
+	assert(2 == 2);
+	assert(2 == 3);  // will cause exception
+	return 0;
+}
+#endif
+
+// Test 4
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+class Base
+{
+public:
+	static const int month;
+};
+
+const int Base::month{12};
+
+int main()
+{
+	cout << Base::month << endl;
 	return 0;
 }
